375e58fac8e423645718701d248534e6396b043e
Eduardo Fleitas <fleitaseduardo141@gmail.com>
2022-07-25 02:54:00 -0400

    Pagina funcional v1.1

--- js/main.js
+++ js/main.js
@@ -3,27 +3,28 @@ const submit = document.querySelector(".submitBtn")
 const thanksScreen = document.querySelector(".thanksScreen")
 const ratingScreen = document.querySelector(".ratingScreen")
 const thanks = document.querySelector(".ratingScore__txt")
-let rating
+
 
 
 for(let i = 0 ; i < selectedNumber.length ; i++){
     
-   
-submit.setAttribute("disabled","true")
-    
-selectedNumber[i].addEventListener('click',(e)=> {
+    submit.setAttribute("disabled","true")
 
-    selectedNumber.forEach((el) => el.classList.remove("ratingNumberSelected"));
-    selectedNumber[i].classList.toggle('ratingNumberSelected');
-    submit.removeAttribute("disabled")
-    
-})};
+    selectedNumber[i].addEventListener('click',(e)=> {
+
+        selectedNumber.forEach((el) => el.classList.remove("ratingNumberSelected"));
+        selectedNumber[i].classList.toggle('ratingNumberSelected');
+        submit.removeAttribute("disabled")
+        
+    })
+
+};
 
 
 submit.addEventListener('click',function() {
-
-    var ratingNumber = document.querySelector(".ratingNumberSelected");
-    rating = ratingNumber.textContent
+    
+    let ratingNumber = document.querySelector(".ratingNumberSelected");
+    let rating = ratingNumber.textContent
     
     let thanksTxt = document.createTextNode(`You selected ${rating} out of 5`);
     thanks.appendChild(thanksTxt);   
